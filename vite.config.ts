9ca05169cf8bc26dd53cbb73b9cedcc75ac9e8fc
zhengsongrui <1245881972@qq.com>
2025-06-27 22:57:15 +0800

    更改环境配置

--- vite.config.ts
+++ vite.config.ts
@@ -4,6 +4,10 @@ import { resolve } from "path"
 
 // https://vite.dev/config/
 export default defineConfig({
+  server: {
+    host: '0.0.0.0', // 使用 0.0.0.0 可以让服务器监听所有网络接口的 IP 地址
+    // port: 3000, // 你希望使用的端口号
+  },
   plugins: [vue()],
   resolve: {
     // 配置路径别名
